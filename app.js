b3ac102c33408813b4044eb71737a73da946523b
introvertisaac <isaacwambiri254@gmail.com>
2024-06-20 18:42:47 +0300

    updated links

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
-const PORT = 3000;
-const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 
 const API_URL = 'https://smartbrainske.com/api/v1';
 const API_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZjIwNWQ2NmFlMDE5MDgxYmMzYTNkZGI1Yzg2MGUxM2JiYmE1MmQ5NWQ1MmRlMTczOTMyMWU4MjA2MTJhMGQ5M2FiMzhlNzVkODc4ZWJmNzEiLCJpYXQiOjE3MTg4OTExMDQuNTY1NzMzLCJuYmYiOjE3MTg4OTExMDQuNTY1NzM2LCJleHAiOjIxOTIxOTAzMDQuNTYxMjM5LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.Vg-Xn57w4MCgT3oYaiJ8-9uFDXrog5pcSi4xne-tF1N9QDDtInC23AVqIAXo5RfSHXMwsM9JcEPOEhRkvt4BvYHHFVf7cOd7RcOO2-fjC7o0zXaqgGWU3xcxwx8GzL6LvYit86FZ0DbX03D4re3hRH2xVwVTs7F4qWIY5XWa4ufPoFn-3T99i9NKqsbbRyO5gDoACQ68_otPpJGJyCpggpHEf-XxuY5T_QFF0b6tw96GLW93t6lZfWyD84y5mG_nRcgbs7ibMum9nUEkkLP5gr4tp_CtaAmVnpMbTbVAAR8BukEYFWL1cOeXABUFv8oGoaO8LFElQ24xdub6tt6rFWyIwj02QKeDUFYNLEhO9yasKcOEaPbA1SXgK7ci6fZk2Rr7E4twDFcnubXuTV3DZ08C_qFxCQUwoSdo11RY50-YcmHQ-rlHyxokSu-972RruIKQ8IjOITwVOqDK5M_7k6VK3Ha26JuEGhmW8dWA1EgBHNHvoZP52XtXY3Gs5ICPAJAQZr6j21CndClqJ2EMVa4QY7AIQJDp1mpDTRiRvseVjqgyehltSaC4504AHPuQ21d--cp3YGHvuZkoEUOKgf_mbo0PMjqvvNJ_d2X1uqP7kduJ7fFxMWy4pwzjIPvFQnVZZ1UX14PSJLntFK57HUUSin1wMGq-Qjd4OmjLDsU';
-
-app.use(bodyParser.json());
-app.use(express.static('public'));
 
 const getHeaders = () => ({
     'Authorization': `Bearer ${API_TOKEN}`,
     'Accept': 'application/json'
 });
 
-// Function to check if an asset is checked out
 const isAssetCheckedOut = async (asset_id) => {
     try {
         const response = await axios.get(`${API_URL}/hardware/${asset_id}`, { headers: getHeaders() });
@@ -27,7 +27,7 @@ const isAssetCheckedOut = async (asset_id) => {
         return asset.status_label.name.toLowerCase() !== 'available';
     } catch (error) {
         console.error('Error fetching asset status:', error);
-        return false; // Assume asset is available if there's an error fetching the status
+        return false;
     }
 };
 
@@ -51,35 +51,25 @@ app.get('/locations', async (req, res) => {
     }
 });
 
-// Endpoint to get assets assigned to a user
-
 app.get('/users/:userId/assets', async (req, res) => {
     const userId = req.params.userId;
     try {
         const response = await axios.get(`${API_URL}/hardware`, {
             headers: getHeaders(),
             params: {
-                assigned_to: userId, // This should filter assets assigned to the specified user
-                limit: 1000 // Adjust limit as necessary
+                assigned_to: userId,
+                limit: 1000
             }
         });
-
-        // Debug log the response data
-        console.log("API Response Data:", response.data);
-
-        // The API may return a data structure where assigned_to is an object, not just an ID.
-        // Ensure we're checking against the correct property.
         const userAssets = response.data.rows.filter(asset => {
             return asset.assigned_to && asset.assigned_to.id === parseInt(userId);
         });
-
         res.json({ total: userAssets.length, rows: userAssets });
     } catch (error) {
         console.error('Error fetching assets for user:', error);
         res.status(500).json({ error: 'Error fetching checked-out assets count' });
     }
 });
-
 
 app.post('/checkout', async (req, res) => {
     const { asset_id, user_id, checkout_at, expected_checkin } = req.body;
@@ -90,9 +80,7 @@ app.post('/checkout', async (req, res) => {
             checkout_at,
             expected_checkin
         };
-        console.log('Checkout payload:', payload);
         const response = await axios.post(`${API_URL}/hardware/${asset_id}/checkout`, payload, { headers: getHeaders() });
-        console.log('Checkout response:', response.data);
         res.json(response.data);
     } catch (error) {
         console.error('Error during checkout:', error.response ? error.response.data : error.message);
@@ -100,7 +88,6 @@ app.post('/checkout', async (req, res) => {
     }
 });
 
-// Checkin endpoint
 app.post('/checkin', async (req, res) => {
     const { asset_id, user_id, checkin_at } = req.body;
     try {
@@ -109,9 +96,7 @@ app.post('/checkin', async (req, res) => {
             assigned_user: user_id,
             checkin_at
         };
-        console.log('Checkin payload:', payload);
         const response = await axios.post(`${API_URL}/hardware/${asset_id}/checkin`, payload, { headers: getHeaders() });
-        console.log('Checkin response:', response.data);
         res.json(response.data);
     } catch (error) {
         console.error('Error during checkin:', error.response ? error.response.data : error.message);
@@ -119,12 +104,10 @@ app.post('/checkin', async (req, res) => {
     }
 });
 
-// Serve the checkin page
 app.get('/checkin', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'checkin.html'));
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server running on https://localhost:${PORT}`);
 });
